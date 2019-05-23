83d35034c37ab391f214426a70116629fa4acd81
jsantos - work <joaomarcosfelixds@gmail.com>
2019-05-23 17:24:05 -0300

    Mapeando Entidades Bean

--- src/br/com/softblue/loucademia/domain/aluno/Endereco.java
+++ src/br/com/softblue/loucademia/domain/aluno/Endereco.java
@@ -1,18 +1,37 @@
 package br.com.softblue.loucademia.domain.aluno;
 
-public class Endereco {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class Endereco implements Serializable {
+
+	@Column(name = "RUA", nullable = false, length = 128)
 	private String rua;
-	
+
+	@Column(name = "NUMERO", nullable = true, length = 6)
 	private Integer numero;
-	
+
+	@Column(name = "COMPLEMENTO", nullable = false, length = 32)
 	private String complemento;
-	
+
+	@Column(name = "CIDADE", nullable = false, length = 64)
 	private String cidade;
-	
+
+	@Column(name = "CEP", nullable = false, length = 8)
 	private Integer cep;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "ESTADO_ID", nullable = false)
 	private Estado estado;
+
+	public Endereco() {
+		this.estado = new Estado();
+	}
 
 	public Estado getEstado() {
 		return estado;
@@ -122,6 +141,5 @@ public class Endereco {
 			return false;
 		return true;
 	}
-	
-	
+
 }
